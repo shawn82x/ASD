b2d3c3987faf6824ff7584d96836f54f589a9231
Shawn <halcyon@fullsail.edu>
2012-01-26 12:38:57 -0500

    Moving files from old app files to asdcouch

--- asdcouch/asdcouch/app/_attachments/main.js
+++ asdcouch/asdcouch/app/_attachments/main.js
@@ -113,6 +113,36 @@ $(document).ready(function() {
 			$('#firlist').listview('refresh');
 		}
 	});
+
+// form validation
+	var clform = $('#newclientform'),
+    clerrorslink = $('#clerrorslink');
+    
+	clform.validate({
+	        invalidHandler: function(form, validator){
+	                clerrorslink.bind('click', function(){
+	                        return false;
+	                });
+	                
+	                var html = '';
+	            
+	                for(var key in validator.submitted){
+	                        var label = $('label[for^="'+ key +'"]').not('[generated]');
+	                        var legend = label.closest('fieldset').find('.ui-controlgroup-label');
+	                        var fieldName = legend.length ? legend.text() : label.text();
+	                        html += '<li>'+ fieldName +'</li>';
+	                };
+	        
+	                $("#newclienterrors ul").html(html);
+	        },
 	
+	        submitHandler: function(){
+	            var data = clform.serializeArray();
+	            parseClientform(data);
+	        }
+	
+	});
+	
+// end	
 });
 
